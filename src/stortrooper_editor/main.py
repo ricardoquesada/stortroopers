0e57bd3f98af9105bc0966dd1df4a118b3f53d45
Ricardo Quesada <ricardoquesada@gmail.com>
2025-12-14 20:04:28 -0800

    build: Enable direct execution of `main.py` by adjusting `sys.path` and `__package__`.

--- src/stortrooper_editor/main.py
+++ src/stortrooper_editor/main.py
@@ -7,6 +7,12 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
+if __name__ == "__main__" and __package__ is None:
+    # Allow running this script directly from the file system
+    # by adding the parent directory to sys.path and forcing the package name.
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    __package__ = "stortrooper_editor"
+
 from .ui import MainWindow
 
 
